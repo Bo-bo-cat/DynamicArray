b699517c6d08adb16c619e51a3777668b8e27469
Bozhena <oshobozena@gmail.com>
2025-12-16 13:42:27 +0200

    fixed readme add graphics

--- new14_11.py
+++ new14_11.py
@@ -1,12 +1,13 @@
 import ctypes
 import math
-
+import time
+import matplotlib.pyplot as plt
 
 class DynamicArray:
     def __init__(self, resize_strategy, capacity=8):
         self._resize_counter = 0
-        self._n = 0 
-        self._capacity = capacity 
+        self._n = 0  # actual elements in array
+        self._capacity = capacity  # initial capacity
         self._array = self._make_array(self._capacity)
         self._resize_strategy = resize_strategy
 
@@ -55,15 +56,46 @@ class DynamicArray:
         return self._capacity
 
 
-arr1 = DynamicArray(DynamicArray.fixed_coefficient, 8)
-arr2 = DynamicArray(DynamicArray.fixed_by_number_coefficient, 16)
-arr3 = DynamicArray(DynamicArray.dynamic_coefficient, 32)
+#тест щоб візуалізувати
+growth_types = ["fixed", "fixed_by_number", "dynamic"]
+sizes = [100, 1000, 5000, 10000, 50000, 100000]
+results = {gt: [] for gt in growth_types}
 
-for i in range(10000):
-    arr1.append(i)
-    arr2.append(i)
-    arr3.append(i)
+for gt_name, strategy in [
+    ("fixed", DynamicArray.fixed_coefficient),
+    ("fixed_by_number", DynamicArray.fixed_by_number_coefficient),
+    ("dynamic", DynamicArray.dynamic_coefficient)
+]:
+    
+    times = []
+    for num in sizes:
+        arr = DynamicArray(strategy, 128)
+        t0 = time.time()
 
-print(arr1.resizecounter())
-print(arr2.resizecounter())
-print(arr3.resizecounter())
+        for i in range(num):
+            arr.append(i)
+
+        t1 = time.time()
+        times.append((t1 - t0) * 1000) 
+        print(f"{gt_name}: {num} елементів - {times[-1]:.2f} мс")
+
+    results[gt_name] = times
+
+# сам графік
+plt.figure(figsize=(12, 7))
+
+for gt_name in growth_types:
+    plt.plot(sizes, results[gt_name], marker='o', linewidth=2, label=gt_name)
+
+plt.xlabel('Кількість елементів')
+plt.ylabel('Час додавання (мс)')
+plt.title('Порівняння швидкості DynamicArray для різних стратегій росту')
+plt.legend()
+
+plt.grid(True, alpha=0.3)
+plt.yscale('log')
+plt.xscale('log')
+
+plt.xticks(sizes, [f'{s:,}' for s in sizes])
+plt.tight_layout()
+plt.show()
